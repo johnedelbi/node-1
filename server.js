b10172a01b8562f38489d7c3b4ee7332f7508b75
John Edelbi <iceflaming@gmail.com>
2023-10-10 13:32:51 +0200

    practice on node fs

--- server.js
+++ server.js
@@ -11,11 +11,43 @@ const filePath = path.join(PATH, 'message.txt');
 const messageToSave = `Hello there,\nwelcome to Node.js `;
 
 // write
-
+fs.writeFile(filePath, messageToSave, 'utf-8', (err) => {
+    if (err) {
+        console.log('there is an error', err);
+    } else {
+        console.log('file saved success');
+    }
+});
 // read
-
+/*
+fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+        console.log('there is an error ', err);
+    } else {
+        console.log(data);
+    }
+});
+*/
 // append
-
+/*
+fs.appendFile(filePath, messageToSave, 'utf-8', (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('data append success');
+    }
+});
+*/
 // open, read , close
 
 // unlink
+/*
+fs.unlink(filePath, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('this file has deleted');
+    }
+});
+
+*/
